8d7f197f45075539227d49e8dea88a3f5cca45ca
EvelynJames9 <98927398+EvelynJames9@users.noreply.github.com>
2022-06-28 21:06:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,8 @@
-
-
-Webcam.set({
-    width:350,
-    height:300,
-    image_format : 'png',
-    png_quality:90
-});
-
-    camera = document.getElementById("camera");
-Webcam.attach( '#camera' );
-
-function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
-    });
+function startClassificatian(){
+    navagator.mediaDevices.getUserMedia({ audio: true});
+    classifier = mlk.soundClassifer('https://teachablemachine.withgoogle.com/models/N9JgzIQF8/', modelReady);
 }
 
-    console.log('ml5 version:', ml5.version);
-
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gyCt6R-y4/model.json',modelLoaded);
-
-    function modelLoaded(){
-        console.log('Model Loaded!');
-    }
-
-    function check(){
-        img = document.getElementById('captured_image');
-        classifier.classify(img, gotResult);
-    }
-
-    function gotResult(error, results) {
-        if(error) {
-            console.error(error);
-        }
-        else {
-            console.log(results);
-            document.getElementById("result_object_name").innerHTML = results[0].label;
-            document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-        }
-    }
+function modelReady(){
+   classifier.classify(gotResults);
+}
